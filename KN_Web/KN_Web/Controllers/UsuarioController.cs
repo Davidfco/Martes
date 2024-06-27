6098d862c4fc97ee36fd0e51cf65f28ffac0ac9f
David Sanchez <davidfsjimenez@gmail.com>
2024-06-26 20:56:28 -0600

    Actualizado a semana 6

---
+++ KN_Web/KN_Web/Controllers/UsuarioController.cs
@@ -0,0 +1,88 @@
+﻿using KN_Web.Entidades;
+using KN_Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KN_Web.Controllers
+{
+    [OutputCache(NoStore = true, VaryByParam = "*", Duration = 0)]
+    public class UsuarioController : Controller
+    {
+        UsuarioModel usuarioM = new UsuarioModel();
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Usuario user)
+        {
+            var respuesta = usuarioM.IniciarSesion(user);
+
+            if (respuesta != null)
+            {
+                Session["NombreUsuario"] = respuesta.Nombre;
+                return RedirectToAction("Home", "Usuario");
+            }
+            else
+            {
+                ViewBag.msj = "Su información no es correcta";
+                return View();
+            }
+        }
+
+
+        [HttpGet]
+        public ActionResult Registro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registro(Usuario user)
+        {
+            var respuesta = usuarioM.RegistrarUsuario(user);
+
+            if (respuesta)
+                return RedirectToAction("Index", "Usuario");
+            else
+            {
+                ViewBag.msj = "Su información no se ha registrado";
+                return View();
+            }
+        }
+
+
+        [FiltroSeguridad]
+        [HttpGet]
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+
+        [FiltroSeguridad]
+        [HttpGet]
+        public ActionResult CerrarSesion()
+        {
+            Session.Clear();
+            return RedirectToAction("Index", "Usuario");
+        }
+
+
+        [FiltroSeguridad]
+        [HttpGet]
+        public ActionResult ConsultarUsuarios()
+        {
+            var respuesta = usuarioM.ConsultarUsuarios();
+            return View(respuesta);
+        }
+
+
+    }
+}
---
+++ KN_Web/KN_Web/Models/UsuarioModel.cs
@@ -0,0 +1,65 @@
+﻿using KN_Web.BaseDatos;
+using KN_Web.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KN_Web.Models
+{
+    public class UsuarioModel
+    {
+        public bool RegistrarUsuario(Usuario user)
+        {
+            var rowsAffected = 0;
+
+            //var tabla = new tUsuario();
+            //tabla.Identificacion = user.Identificacion;
+            //tabla.Nombre = user.Nombre;
+            //tabla.Correo = user.Correo;
+            //tabla.Contrasenna = user.Contrasenna;
+            //tabla.Estado = true;
+            //tabla.IdRol = 1;
+
+            //using (var context = new MARTES_BDEntities())
+            //{
+            //    context.tUsuario.Add(tabla);
+            //    rowsAffected = context.SaveChanges();
+            //}
+
+            using (var context = new MARTES_BDEntities())
+            {
+                rowsAffected = context.RegistrarUsuario(user.Identificacion, user.Nombre, user.Correo, user.Contrasenna);
+            } 
+
+            return (rowsAffected > 0 ? true : false);
+        }
+
+        public IniciarSesion_Result IniciarSesion(Usuario user)
+        {
+            //using (var context = new MARTES_BDEntities())
+            //{
+            //    rowsCount = (from x in context.tUsuario
+            //                     where x.Correo == user.Correo
+            //                     && x.Contrasenna == user.Contrasenna
+            //                     && x.Estado == true
+            //                     select x).ToList().Count();
+            //}
+
+            using (var context = new MARTES_BDEntities())
+            {
+                return context.IniciarSesion(user.Correo, user.Contrasenna).FirstOrDefault();
+            }
+        }
+
+        public List<tUsuario> ConsultarUsuarios()
+        {
+            using (var context = new MARTES_BDEntities())
+            {
+                return (from x in context.tUsuario
+                             select x).ToList();
+            }
+        }
+
+    }
+}
